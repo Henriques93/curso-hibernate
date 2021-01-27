5715494005e405ec30240dce4d9b8aa65dc16645
Henriques <henriquesxp@gmail.com>
2021-01-27 09:15:07 -0300

    Primeiro commit

---
+++ src/main/java/br/com/henriques/entity/Cliente.java
@@ -0,0 +1,70 @@
+package br.com.henriques.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int id;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private String rg;
+	
+	@Column
+	private String cpf;
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(String nome, String rg, String cpf) {
+		super();
+		this.nome = nome;
+		this.rg = rg;
+		this.cpf = cpf;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
